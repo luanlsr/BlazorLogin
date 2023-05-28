414385d33b5df0b7e91c8dcabe7055d30963a823
luanlsr <luanfswd@gmail.com>
2023-05-27 22:11:05 -0300

    configurado páginas do admin

--- AppContas.Web/Program.cs
+++ AppContas.Web/Program.cs
@@ -1,7 +1,9 @@
 using AppContas.Web;
+using AppContas.Web.Services;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using static System.Net.WebRequestMethods;
+using BlazorBootstrap;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -14,5 +16,12 @@ builder.Services.AddScoped(sp => new HttpClient {
     //BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
     BaseAddress = new Uri("http://appcontasblazor-001-site1.htempurl.com/api/")
 });
+//Registrando os serviços da biblioteca Blazored.LocalStorage
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazorBootstrap();
+
+//Mapeando injeção de dependência da classe AuthService
+builder.Services.AddTransient<AuthService>();
 
 await builder.Build().RunAsync();
+
---
+++ AppContas.Web/Services/AuthService.cs
@@ -0,0 +1,66 @@
+﻿using AppContas.Web.Responses;
+using Blazored.LocalStorage;
+using Newtonsoft.Json;
+//using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+namespace AppContas.Web.Services
+{
+    /// <summary>
+    /// Classe d serviço para operações de autenticação de usuários
+    /// </summary>
+    public class AuthService
+    {
+        private readonly ILocalStorageService _localStorageService;
+        private const string _key = "appconta-auth";
+
+        public AuthService(ILocalStorageService localStorageService)
+        {
+            _localStorageService = localStorageService;
+        }
+
+        /// <summary>
+        /// Método para salvar os dados de autenticação do usuário no LocalStorage
+        /// </summary>
+        public async Task SignIn(AutenticarResponse data)
+        {
+            var json = JsonConvert.SerializeObject(data);
+            await _localStorageService.SetItemAsync(_key, json);
+        }
+
+        /// <summary>
+        /// Método para verificar se o usuário está autenticado
+        /// </summary>
+        /// <returns></returns>
+        public async Task<bool> IsSigningIn()
+        {
+            var json = await _localStorageService.GetItemAsync<string>(_key);
+            if(!string.IsNullOrEmpty(json))
+            {
+                var data = JsonConvert.DeserializeObject<AutenticarResponse>(json);
+                return data.DataExpiracao >= DateTime.Now;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Método para retornar os dados de autenticação do usuário do LocalStorage
+        /// </summary>
+        /// <returns></returns>
+        public async Task<AutenticarResponse> GetData()
+        {
+            var json = await _localStorageService.GetItemAsync<string>(_key);
+            return JsonConvert.DeserializeObject<AutenticarResponse>(json);
+        }
+
+        /// <summary>
+        /// Apagar as informações de autenticação do usuário do LocalStorage
+        /// </summary>
+        /// <returns></returns>
+        public async Task SignOut()
+        {
+            await _localStorageService.RemoveItemAsync(_key);
+        }
+
+    }
+}
